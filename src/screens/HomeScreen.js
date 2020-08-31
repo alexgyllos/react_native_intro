b6bef8a00a13932a710db21766e1d5bbbec664cc
Alex Gyllos <alex.gyllos@gmail.com>
2020-08-31 13:14:53 +0300

    adds navigate function to nav buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       <Button 
-        onPress={() => console.log('button pressed')}
+        onPress={() => props.navigation.navigate('Components')}
         title="Go To Components Demo"/>
-      <TouchableOpacity onPress={() => console.log('list pressed')}>
+      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go To List Demo</Text>
       </TouchableOpacity>
     </View>
