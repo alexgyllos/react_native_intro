b7f57610a807b9afd2dec3ddff7eaad5addc79e5
Alex Gyllos <alex.gyllos@gmail.com>
2020-08-31 12:49:22 +0300

    setup multiple components

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+
+    const name = 'Alex';
+
+    return <View>
+            <Text style={styles.textStyle}>Getting started with React Native!</Text>
+            <Text style={styles.textStyle}>My name is {name}</Text>
+           </View>
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 45
+    }
+});
+
+export default ComponentsScreen;
